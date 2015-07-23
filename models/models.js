dbbc114b2b4719379c92c35dd983780976b17bd6
cfibla <charliesuper@hotmail.com>
2015-07-23 14:50:58 +0200

    Despliegue DB en Heroku

---
+++ models/models.js
@@ -0,0 +1,47 @@
+var path=require('path');
+
+//Postgres DATABASE_URL = Postgres://user:passwd@host:port/database
+//Sqlite DATABASE_URL = sqlite://:@:/
+var url=process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var protocol = (url[1]||null);
+var dialect = (url[1]||null);
+var user = (url[2]||null);
+var pwd = (url[3]||null);
+var host = (url[4]||null);
+var port = (url[5]||null);
+var DB_name = (url[6]||null);
+var storage = process.env.DATABASE_STORAGE;
+
+//Cargar modelo ORM
+var Sequelize=require('sequelize');
+
+//Usar DB SQlite o Postgres
+var sequelize=new Sequelize(DB_name, user, pwd,
+	{
+		dialect: dialect,
+		protocol: protocol,
+		port: port,
+		host: host,
+		storage: storage, 	//soloSQlite (.env)
+		omitNull: true		//solo Postgres
+	}
+	);
+
+//Importar la definición de la tabla Quiz en quiz.js
+var Quiz=sequelize.import(path.join(__dirname,'quiz'));
+exports.Quiz=Quiz;//exportar definición de tabla Quiz
+
+//sequelize.sync() crea e inicializa tabla de preguntas en la DB
+sequelize.sync().success(function(){
+	//sucess(...) ejecuta el manejador una vez creada la tabla
+	Quiz.count().success(function(count){
+		if(count===0)//la tabla se inicializa solo si está vacia
+		{
+			Quiz.create({
+				pregunta: 'Capital de Italia',
+				respuesta: 'Roma'
+			})
+			.success(function(){console.log('Base de datos inicializada')});
+		};
+	});
+});

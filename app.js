a97e40c0760ab833a883c985585aeba282478835
cfibla <charliesuper@hotmail.com>
2015-07-28 17:45:45 +0200

    tema 8 crear pregunta y validar

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
-        error: {}
+        error: {},
         errors:[]
     });
 });

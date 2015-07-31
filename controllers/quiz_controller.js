d2a2be01e38fb865b99e11a24cf5ef3abc878d23
cfibla <charliesuper@hotmail.com>
2015-07-31 13:23:34 +0200

    v1

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -15,11 +15,29 @@ exports.load = function(req, res, next, quizId){
 
 //GET /quizes
 exports.index = function (req, res) {
-	models.Quiz.findAll().then(function (quizes){
+	if(req.query.search){
+	var filtro=(req.query.search || '').replace(' ','%');
+	models.Quiz.findAll({where:['pregunta like ?','%'+filtro+'%'],order:'pregunta ASC'})
+	.then(function(quizes){
 		res.render('quizes/index', {quizes: quizes, errors:[]});
-	}
-	).catch(function(error){next(error);})
-};
+	}).catch(function(error){next(error);});
+
+} else {
+
+	if(req.query.tema){
+	var filtro=(req.query.tema || '')
+	models.Quiz.findAll({where:['tema like ?','%'+filtro+'%'],order:'tema ASC'})
+	.then(function(quizes){
+		res.render('quizes/index', {quizes: quizes, errors:[]});
+	}).catch(function(error){next(error);});
+
+} else {
+
+	models.Quiz.findAll()
+	.then(function(quizes){
+		res.render('quizes/index', {quizes: quizes, errors:[]});
+	}).catch(function(error){next(error);});
+}
 
 //GET /quizes/:id
 exports.show = function (req, res){

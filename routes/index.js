c906dd521ace6bcc59c55144e9f291ee21926f71
cfibla <charliesuper@hotmail.com>
2015-08-10 20:04:34 +0200

    comentarios v5

--- routes/index.js
+++ routes/index.js
@@ -27,7 +27,7 @@ router.put('/quizes/:quizId(\\d+)', quizController.update);
 router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
 
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
-router.post('quizes/:quizId(\\d+)/comments', commentController.create);
+router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
 
 router.get('/author', function (req, res, next){
   res.render('author',{errors:[]})

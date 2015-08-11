58f21aa8e21f73507762222dbb8c75c946ece8c1
cfibla <charliesuper@hotmail.com>
2015-08-11 12:58:29 +0200

    login v1

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -16,7 +16,7 @@ exports.create = function (req, res){
 	userController.autenticar(login, password, function(error, user){
 
 		if(error){
-			req.session.errors = [{"message: 'Se ha producido un error' + error"}];
+			req.session.errors = [{"message": 'Se ha producido un error ' + error}];
 			res.redirect("/login");
 			return;
 		}

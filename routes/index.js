a8ed1da1ef9e96ab166bd5d7811828b7c857a168
cfibla <charliesuper@hotmail.com>
2015-08-12 13:49:43 +0200

    publicar v3

--- routes/index.js
+++ routes/index.js
@@ -35,7 +35,7 @@ router.delete('/quizes/:quizId(\\d+)',		sessionController.loginRequired,	quizCon
 
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
-router.put('/quizes/:quizId(\\d+)/comments/commentId(\\d+)/publish',
+router.get('/quizes/:quizId(\\d+)/comments/commentId(\\d+)/publish',
 											sessionController.loginRequired, commentController.publish);
 
 router.get('/author', function (req, res, next){

364607c66d4a6adb04fb376031ca35198a68cabb
cfibla <charliesuper@hotmail.com>
2015-08-12 14:45:18 +0200

    publicar v7

--- routes/index.js
+++ routes/index.js
@@ -32,10 +32,9 @@ router.delete('/quizes/:quizId(\\d+)',		sessionController.loginRequired,	quizCon
 	//ANULADA - router.get('/quizes/answer', quizController.answer);
 
 //Definición de rutas de comentarios
-router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
-router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
-router.get('/quizes/:quizId(\\d+)/comments/commentId(\\d+)/publish',
-											sessionController.loginRequired,	commentController.publish);
+router.get('/quizes/:quizId(\\d+)/comments/new',						commentController.new);
+router.post('/quizes/:quizId(\\d+)/comments',							commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/commentId(\\d+)/publish',	sessionController.loginRequired,	commentController.publish);
 
 router.get('/author', function (req, res, next){
   res.render('author',{errors:[]})
